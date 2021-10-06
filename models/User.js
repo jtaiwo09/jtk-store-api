34458fccee59e8feb6f8f1114c0ec20607f7b0d1
Taiwo <taiwokelvin@gmail.com>
2021-10-06 03:48:32 -0700

    commit

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     pwd: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false }
+    isAdmin: { type: Boolean, default: false },
+    img: { type: String }
 }, { timestamps: true })
 
 module.exports = mongoose.model('User', UserSchema);
